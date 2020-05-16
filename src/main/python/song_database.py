c8c6010b367dc13d73067bf63f1fbad3609ab4de
Michael Knyazev <knyazev.michael@gmail.com>
2020-05-16 17:46:37 +0300

    do not store db object, use the default connection instead

--- src/main/python/song_database.py
+++ src/main/python/song_database.py
@@ -3,42 +3,37 @@ from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
 class SongDatabase():
     def __init__(self, db_path):
-        self._db = QSqlDatabase.addDatabase('QSQLITE')
-        self._db.setDatabaseName(db_path)
-        if self._db.open():
-            query = QSqlQuery(self._db)
-            query.exec(('CREATE TABLE IF NOT EXISTS songs '
-                        '(track_id INTEGER NOT NULL UNIQUE, '
-                        'album_id INTEGER NOT NULL, '
-                        'title TEXT, '
-                        'artist TEXT, '
-                        'PRIMARY KEY (track_id));'))
-            query.exec(('CREATE TABLE IF NOT EXISTS playlist '
-                        '(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, '
-                        'position INTEGER NOT NULL, '
-                        'is_new INTEGER NOT NULL, '
-                        'title INTEGER NOT NULL, '
-                        'artist INTEGER NOT NULL);'))
-        else:
-            print(self._db.lastError())
+        db_ = QSqlDatabase.addDatabase('QSQLITE')
+        db_.setDatabaseName(db_path)
+        if not db_.open():
+            print(db_.lastError())
+            return
+
+        db_.exec(('CREATE TABLE IF NOT EXISTS songs '
+                  '(track_id INTEGER NOT NULL UNIQUE, '
+                  'album_id INTEGER NOT NULL, '
+                  'title TEXT, '
+                  'artist TEXT, '
+                  'PRIMARY KEY (track_id));'))
+        db_.exec(('CREATE TABLE IF NOT EXISTS playlist '
+                  '(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, '
+                  'position INTEGER NOT NULL, '
+                  'is_new INTEGER NOT NULL, '
+                  'title INTEGER NOT NULL, '
+                  'artist INTEGER NOT NULL);'))
 
     def has_song(self, track_id) -> bool:
-        if not self._db.isOpen():
-            return None
-        query = QSqlQuery(self._db)
+        query = QSqlQuery()
         if not query.exec('SELECT 1 FROM songs WHERE track_id = {0}'.format(track_id)):
             return None
         return query.next()
 
     def add_song(self, track_id, album_id, title, artist_info) -> None:
-        if not self._db.isOpen():
-            return
-
         artists = []
         for artist in artist_info:
             artists.append(artist.name)
         artist = ', '.join(artists)
-        query = QSqlQuery(self._db)
+        query = QSqlQuery()
         query.prepare(('INSERT INTO songs (track_id, album_id, title, artist) '
                        'VALUES (:track_id, :album_id, :title, :artist)'))
         query.bindValue(':track_id', track_id)
@@ -49,20 +44,17 @@ class SongDatabase():
 
     def get_songs(self, track_ids) -> List[str]:
         songs = list()
-        if not self._db.isOpen():
-            return songs
+        query = QSqlQuery()
         for track_id in track_ids:
-            query = self._db.exec(('SELECT artist, title FROM songs '
-                                   'WHERE track_id = {0}').format(track_id))
+            query.exec(('SELECT artist, title FROM songs '
+                        'WHERE track_id = {0}').format(track_id))
             if query.next():
                 songs.append('{0} - {1}'.format(query.value(0), query.value(1)))
         return songs
 
     def get_playlist(self) -> Dict[int, int]:
         songs = dict()
-        if not self._db.isOpen():
-            return songs
-        query = QSqlQuery(self._db)
+        query = QSqlQuery()
         query.setForwardOnly(True)
         query.exec('SELECT title, position FROM playlist')
         while query.next():
@@ -70,9 +62,7 @@ class SongDatabase():
         return songs
 
     def add_tracks(self, track_ids, positions):
-        if not self._db.isOpen():
-            return
-        query = QSqlQuery(self._db)
+        query = QSqlQuery()
         query.prepare(('INSERT INTO playlist (position, is_new, title, artist)'
                        ' VALUES (?, ?, ?, ?)'))
         query.addBindValue(positions)
@@ -82,16 +72,14 @@ class SongDatabase():
         query.execBatch()
 
     def remove_tracks(self, track_ids) -> None:
-        if not self._db.isOpen():
-            return
+        query = QSqlQuery()
         for track_id in track_ids:
-            self._db.exec(('DELETE FROM playlist '
-                           'WHERE title = {0}').format(track_id))
+            query.exec(('DELETE FROM playlist '
+                        'WHERE title = {0}').format(track_id))
 
     def update_tracks_position(self, position_by_track) -> None:
-        if not self._db.isOpen():
-            return
+        query = QSqlQuery()
         for track_id, position in position_by_track:
-            self._db.exec(('UPDATE playlist SET position = {0} '
-                           'WHERE title = {1}')
-                          .format(position, track_id))
+            query.exec(('UPDATE playlist SET position = {0} '
+                        'WHERE title = {1}')
+                       .format(position, track_id))
